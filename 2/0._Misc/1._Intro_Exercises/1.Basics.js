dc89a0c6e70c297392925676947b26245a4036b7
omar0190 <61865216+omar0190@users.noreply.github.com>
2021-02-04 15:52:30 +0100

    a

---
+++ 2/0._Misc/1._Intro_Exercises/1.Basics.js
@@ -0,0 +1,30 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constants
+
+const firstName = "firstName";
+const lastName = "lastName";
+
+console.log("My first name is " + firstName + " And my last name is "  + lastName)
+
+// EXERCISE
+// show in the console
+// My first name is Omar and my last name is Atik
+
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2020";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2021
+// You cannot touch line 1 or 
+
+const result = year + number;
+console.log(parseInt(year) + number)
+
+
+// --------------------------------------
---
+++ 2/0._Misc/1._Intro_Exercises/2.Strings.js
@@ -0,0 +1,65 @@
+// --------------------------------------
+// Exercise 3 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+
+
+console.log(Number(numberOne) + Number(numberTwo));
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 4 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+result = (Number(anotherNumberOne) + Number(anotherNumberTwo));
+console.log(result.toFixed(2))
+// --------------------------------------
+// Exercise 5 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+plus = one + two + three
+average = plus / 3
+console.log(average.toFixed(5))
+
+
+
+// --------------------------------------
+// Exercise 6 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+const gettingC = letters.charAt(2);
+console.log(gettingC)
+
+
+// --------------------------------------
+// Exercise 7 - Replace
+
+const fact = "You are learning javascript!";
+const changingJ = fact.replace("javascript", "Javascript")
+
+console.log(changingJ)
+
+// capitalize the J in Javascript
+
+
+// --------------------------------------
+
+
+

08035b8006d5e73f190ac4c461a0fa81aa9abebc
omar0190 <61865216+omar0190@users.noreply.github.com>
2021-02-11 12:01:47 +0100

    a

---
+++ 2/0._Misc/1._Intro_Exercises/3.Objects.js
@@ -0,0 +1,54 @@
+// --------------------------------------
+// Objects
+// --------------------------------------
+// Exercise 1 - Retrieve value from object by key
+
+const myObj = {"message": "Hello, earthling! I bring peace."};
+
+
+// Log the message 
+
+let val = myObj.message;
+
+console.log(val)
+
+// --------------------------------------
+// Exercise 2 - Defining an object. 
+
+const myObj1 = {
+    name: "Omar",
+    age: 23
+}
+console.log(myObj1);
+
+// Create an object that has your name and age. 
+
+
+// --------------------------------------
+// Exercise 3 - Add a property 
+
+const stackOverflow = {
+    isAllowed: true
+};
+
+console.log(stackOverflow.isAllowed);
+
+
+// make a rule called isAllowed and let the value be true
+
+// --------------------------------------
+// Exercise 4 - Remove a property 
+
+const thisSong = {"description": "The best song in the world."}
+
+// remove the property "description" and add a property called "about" that should say "Just a tribute." 
+
+delete thisSong.description;
+
+    thisSong.new = " Just a tribute. " 
+
+    console.log(thisSong.new)
+
+// --------------------------------------
+
+

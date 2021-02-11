2767f2464ad9ae940514785342d05feb4e09eec5
omar0190 <61865216+omar0190@users.noreply.github.com>
2021-02-11 15:49:11 +0100

    a

---
+++ 2/1._First_Server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send ({message: "First call to /"});
+});
+
+app.get("/anotherpath", (req, res) => {
+    res.send({data: "some other things"});
+});
+
+app.listen(8080);
+
